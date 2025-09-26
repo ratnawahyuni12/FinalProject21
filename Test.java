ff11f3b81de8162d66ab11b0b2f5b6bbe4671ee6
“aretaneilaputri <retaaneila@gmail.com>
2025-09-26 17:30:05 +0700

    hallooo

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama");
         System.out.println("hai, ratna");
+        System.out.println("haiii");
     }
 }
